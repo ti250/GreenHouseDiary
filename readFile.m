11fa449f0c47f5930d02c56f671cba1719e6183b
Nenyams <neneyamasaki@dhcp-10-248-150-229.eduroam.wireless.private.cam.ac.uk>
2018-03-10 17:05:39 +0000

    Some edits

--- readFile.m
+++ readFile.m
@@ -1,7 +1,18 @@
+clear;
+set(0,'DefaultFigureVisible','off')
+%Start of the code
 breakdown = csvread('dummyData.csv',1,1);
 [m,n] = size(breakdown);
-labels = {'Bathroom','Shower','Kitchen','Others'};
-pie(breakdown(:,n),labels)
-
-
-
+labels = {'Others','Bathroom','Kitchen','Shower'};
+data = breakdown(:,n);
+pie = pie(data);
+colormap([0.996 0.813 0.551;     
+          0.961 0.512 0.438;      
+          0.992 0.668 0.723;     
+          0.809 0.590 0.840]); 
+T = pie(strcmpi(get(pie,'Type'),'text'));
+P = cell2mat(get(T,'Position'));
+set(T,{'Position'},num2cell(P*0.6,2));
+text(P(:,1),P(:,2),labels(:));
+set(gcf, 'color', 'none');
+saveas(gcf,'images/breakdown.png')
